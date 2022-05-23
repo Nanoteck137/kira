477a626ec01b5be739c87b44536b75252bd7b383
Patrik M. Rosenström <patrik.millvik@gmail.com>
2022-05-24 00:21:59 +0200

    Loading program headers to memory

--- src/main.rs
+++ src/main.rs
@@ -23,11 +23,26 @@ fn main() {
     let e = elf::Elf::parse(&file_data).unwrap();
     println!("Elf: {:#?}", e);
 
-    let memory = vec![0; 100 * 1024 * 1024];
+    let mut memory = vec![0; 100 * 1024 * 1024];
+    const MEMORY_OFFSET: u64 = 0x80000000;
+
+    let mut write_memory = |addr: u64, val: u8| {
+        let memory_addr = addr.wrapping_sub(MEMORY_OFFSET);
+        // println!("Write Memory: {:#x} -> {:#x}", memory_addr, val);
+        memory[memory_addr as usize] = val;
+    };
 
     for program_header in e.program_header_iter() {
-        let data = e.program_header_data(program_header)
-            .expect("Failed to get program header data");
-        println!("{:#x?}: {:#x}", program_header, data.len());
+        if program_header.typ() == elf::ProgramHeaderTyp::Load {
+            let data = e.program_header_data(program_header)
+                .expect("Failed to get program header data");
+            println!("{:#x?}: {:#x}", program_header, data.len());
+
+            for index in 0..data.len() {
+                // println!("Writing: {}");
+                // memory[index] = ;
+                write_memory(program_header.vaddr() + index as u64, data[index]);
+            }
+        }
     }
 }
