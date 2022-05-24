a2dc88ad1c7eda134062cfa2d6b6b9df772587f3
Patrik M. Rosenström <patrik.millvik@gmail.com>
2022-05-24 10:25:34 +0200

    Writing to memory

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,31 @@ fn read_file_to_vec<P>(path: P) -> Vec<u8>
     result
 }
 
+struct Memory {
+    memory: Vec<u8>,
+}
+
+impl Memory {
+    fn new(size: usize) -> Self {
+        Self {
+            memory: vec![0; size],
+        }
+    }
+
+    fn write_u8(&mut self, addr: usize, value: u8) {
+        self.memory[addr] = value;
+    }
+
+    fn read_u32(&mut self, addr: usize) -> u32 {
+        let a = self.memory[addr + 0] as u32;
+        let b = self.memory[addr + 1] as u32;
+        let c = self.memory[addr + 2] as u32;
+        let d = self.memory[addr + 3] as u32;
+
+        (d << 24) | (c << 16) | (b << 8) | a
+    }
+}
+
 fn main() {
     let path = "/opt/riscv/target/share/riscv-tests/isa/rv64ui-v-add";
     let file_data = read_file_to_vec(path);
@@ -23,13 +48,13 @@ fn main() {
     let e = elf::Elf::parse(&file_data).unwrap();
     println!("Elf: {:#?}", e);
 
-    let mut memory = vec![0; 100 * 1024 * 1024];
+    let mut memory = Memory::new(100 * 1024 * 1024);
     const MEMORY_OFFSET: u64 = 0x80000000;
 
     let mut write_memory = |addr: u64, val: u8| {
         let memory_addr = addr.wrapping_sub(MEMORY_OFFSET);
         // println!("Write Memory: {:#x} -> {:#x}", memory_addr, val);
-        memory[memory_addr as usize] = val;
+        memory.write_u8(memory_addr as usize, val);
     };
 
     for program_header in e.program_header_iter() {
@@ -45,4 +70,7 @@ fn main() {
             }
         }
     }
+
+    let val = memory.read_u32(e.entry() as usize - MEMORY_OFFSET as usize);
+    println!("Val: {:#x}", val);
 }
